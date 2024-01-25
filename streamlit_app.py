fd474c3222aa0ce2deef16a691d9eb6d54f7094f
GeMa300 <157598945+GeMa300@users.noreply.github.com>
2024-01-25 08:48:04 +0100

    Update streamlit_app.py
    typooo

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here for troubleshooting
-streamlit.STOP()
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
